5cf0cb01f52c96286c0c35d27c5592a43ba8d3c9
pierre gosselin <pierre.gosselin@orange.fr>
2019-08-26 14:51:27 +0200

    add fixtures

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -45,6 +45,11 @@ class Product
      */
     private $slug;
 
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="products")
+     */
+    private $user;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -102,6 +107,18 @@ class Product
         }
 
         $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
 
         return $this;
     }
